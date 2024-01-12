3784ba278c7f306e420f0fdfca309adbff07aa3e
plasma <119145022+Amethystic@users.noreply.github.com>
2024-01-12 00:31:40 -0600

    Revert "Code"
    This reverts commit 2d2904ecd0b44c1e01ff33c15a1c03e5b97a9249.

--- simple-clicker/menu.cpp
+++ simple-clicker/menu.cpp
@@ -18,12 +18,12 @@ ImVec4 firstChildColor = ImColor(17, 17, 17, 255);
 ImVec4 seconedChildBorder = ImColor(42, 42, 42, 255);
 ImVec4 seconedChildColor = ImColor(21, 21, 21, 255);
 
-static const char* status[]{ "S.A.R.S", "Lethal Company", "CS:GO"};
+static const char* status[]{ "Game 1", "Game 2" };
 static int selected_cheat = 0;
 
 class initWindow {
 public:
-    const char* window_title = "Catnip.lol";
+    const char* window_title = "YourCheatDomain.gg";
     ImVec2 window_size{ 400, 205 };
     DWORD window_flags = ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoResize;
 } iw;
@@ -32,61 +32,19 @@ void load_styles()
 {
     ImVec4* colors = ImGui::GetStyle().Colors;
     {
-        colors[ImGuiCol_Text] = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
-        colors[ImGuiCol_TextDisabled] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_WindowBg] = ImVec4(0.04f, 0.04f, 0.04f, 1.00f);
-        colors[ImGuiCol_ChildBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-        colors[ImGuiCol_PopupBg] = ImVec4(0.08f, 0.08f, 0.08f, 0.94f);
-        colors[ImGuiCol_Border] = ImVec4(0.43f, 0.43f, 0.50f, 0.50f);
-        colors[ImGuiCol_BorderShadow] = ImVec4(0.18f, 0.18f, 0.18f, 1.00f);
-        colors[ImGuiCol_FrameBg] = ImVec4(0.06f, 0.06f, 0.06f, 1.00f);
-        colors[ImGuiCol_FrameBgHovered] = ImVec4(0.00f, 0.00f, 0.00f, 0.40f);
-        colors[ImGuiCol_FrameBgActive] = ImVec4(0.62f, 0.26f, 0.98f, 0.67f);
-        colors[ImGuiCol_TitleBg] = ImVec4(0.05f, 0.05f, 0.05f, 1.00f);
-        colors[ImGuiCol_TitleBgActive] = ImVec4(0.22f, 0.12f, 0.29f, 1.00f);
+        colors[ImGuiCol_WindowBg] = ImVec4(0.11f, 0.11f, 0.11f, 1.00f);
+        colors[ImGuiCol_Border] = ImVec4(0.08f, 0.08f, 0.08f, 1.00f);
+        colors[ImGuiCol_TitleBg] = ImVec4(0.08f, 0.08f, 0.08f, 1.00f);
+        colors[ImGuiCol_TitleBgActive] = ImVec4(0.18f, 0.45f, 0.87f, 1.00f);
         colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.08f, 0.08f, 0.08f, 1.00f);
-        colors[ImGuiCol_MenuBarBg] = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
-        colors[ImGuiCol_ScrollbarBg] = ImVec4(0.02f, 0.02f, 0.02f, 0.53f);
-        colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.31f, 0.31f, 0.31f, 1.00f);
-        colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.41f, 0.41f, 0.41f, 1.00f);
-        colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.51f, 0.51f, 0.51f, 1.00f);
-        colors[ImGuiCol_CheckMark] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_SliderGrab] = ImVec4(0.75f, 0.44f, 1.00f, 1.00f);
-        colors[ImGuiCol_SliderGrabActive] = ImVec4(0.80f, 0.56f, 1.00f, 1.00f);
-        colors[ImGuiCol_Button] = ImVec4(0.08f, 0.08f, 0.08f, 1.00f);
-        colors[ImGuiCol_ButtonHovered] = ImVec4(0.30f, 0.11f, 0.45f, 1.00f);
-        colors[ImGuiCol_ButtonActive] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_Header] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_HeaderHovered] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_HeaderActive] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_Separator] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_SeparatorHovered] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_SeparatorActive] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_ResizeGrip] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.57f, 0.22f, 0.85f, 1.00f);
-        colors[ImGuiCol_ResizeGripActive] = ImVec4(0.76f, 0.46f, 1.00f, 1.00f);
-        colors[ImGuiCol_Tab] = ImVec4(0.07f, 0.07f, 0.07f, 1.00f);
-        colors[ImGuiCol_TabHovered] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_TabActive] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_TabUnfocused] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_DockingPreview] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_DockingEmptyBg] = ImVec4(0.20f, 0.20f, 0.20f, 1.00f);
-        colors[ImGuiCol_PlotLines] = ImVec4(0.61f, 0.61f, 0.61f, 1.00f);
-        colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
-        colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
-        colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
-        colors[ImGuiCol_TableHeaderBg] = ImVec4(0.19f, 0.19f, 0.20f, 1.00f);
-        colors[ImGuiCol_TableBorderStrong] = ImVec4(0.31f, 0.31f, 0.35f, 1.00f);
-        colors[ImGuiCol_TableBorderLight] = ImVec4(0.23f, 0.23f, 0.25f, 1.00f);
-        colors[ImGuiCol_TableRowBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-        colors[ImGuiCol_TableRowBgAlt] = ImVec4(1.00f, 1.00f, 1.00f, 0.06f);
-        colors[ImGuiCol_TextSelectedBg] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_DragDropTarget] = ImVec4(1.00f, 1.00f, 0.00f, 0.90f);
-        colors[ImGuiCol_NavHighlight] = ImVec4(0.66f, 0.26f, 0.98f, 1.00f);
-        colors[ImGuiCol_NavWindowingHighlight] = ImVec4(1.00f, 1.00f, 1.00f, 0.70f);
-        colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.20f);
-        colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.35f);
+        colors[ImGuiCol_ButtonHovered] = ImVec4(0.07f, 0.32f, 0.71f, 1.00f);
+        colors[ImGuiCol_ButtonActive] = ImVec4(0.00f, 0.79f, 1.00f, 1.00f);
+        colors[ImGuiCol_Button] = ImColor(21, 21, 21, 255);
+        colors[ImGuiCol_FrameBg] = ImVec4(0.21f, 0.21f, 0.21f, 0.54f);
+        colors[ImGuiCol_FrameBgHovered] = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
+        colors[ImGuiCol_Header] = ImVec4(0.00f, 0.00f, 0.00f, 0.31f);
+        colors[ImGuiCol_HeaderHovered] = ImVec4(0.00f, 0.00f, 0.00f, 0.31f);
+        colors[ImGuiCol_HeaderActive] = ImVec4(0.00f, 0.00f, 0.00f, 0.31f);
     }
 
     ImGuiStyle* style = &ImGui::GetStyle();
@@ -118,6 +76,7 @@ void menu::render()
 
         ImGui::Begin(iw.window_title, &globals.active, iw.window_flags);
         {
+
             ImGui::PushStyleColor(ImGuiCol_Border, firstChildBorder);
             ImGui::PushStyleColor(ImGuiCol_ChildBg, firstChildColor);
 
@@ -148,9 +107,9 @@ void menu::render()
                     ImGui::TextDisabled("Cheat Info");
                     if (selected_cheat == 0) // Game 1
                     {
-                        ImGui::Text("Status: ");
+                        ImGui::Text("Status:");
                         ImGui::SameLine();
-                        ImGui::TextDisabled("Detected | W.I.P");
+                        ImGui::TextDisabled("?");
 
                         ImGui::Text("Update Ver:");
                         ImGui::SameLine();
@@ -158,19 +117,9 @@ void menu::render()
                     }
                     else if (selected_cheat == 1) // Game 2
                     {
-                        ImGui::Text("Status: ");
+                        ImGui::Text("Status:");
                         ImGui::SameLine();
-                        ImGui::TextDisabled("W.I.P");
-
-                        ImGui::Text("Update Ver:");
-                        ImGui::SameLine();
-                        ImGui::TextDisabled("0.0.0.0");
-                    }
-                    else if (selected_cheat == 2) // Game 3
-                    {
-                        ImGui::Text("Status: ");
-                        ImGui::SameLine();
-                        ImGui::TextDisabled("Detected | W.I.P");
+                        ImGui::TextDisabled("?");
 
                         ImGui::Text("Update Ver:");
                         ImGui::SameLine();
@@ -184,26 +133,18 @@ void menu::render()
 
                 if (selected_cheat == 0) // Game 1
                 {
-                    //if (ImGui::Button("Inject", ImVec2(ImGui::GetWindowWidth() - 16, 23)))
-                    //{
-                    //    inject(L"Super Animal Royale.exe", L"test.dll");
-                    //    exit(0); // if u want u can ctrl + k + c that exit func or delete it if u want to
-                    //}
+                    if (ImGui::Button("Inject", ImVec2(ImGui::GetWindowWidth() - 16, 23)))
+                    {
+                        inject(L"YourGameName.Exe", L"YourCheatName.dll");
+                        /*exit(0);*/ // if u want u can ctrl + k + c that exit func or delete it if u want to
+                    }
                 }
-                if (selected_cheat == 1) // Game 1
+                else if (selected_cheat == 1) // Game 2
                 {
                     //if (ImGui::Button("Inject", ImVec2(ImGui::GetWindowWidth() - 16, 23)))
                     //{
-                    //    inject(L"Lethal Company.exe", L"test.dll");
-                    //    exit(0); // if u want u can ctrl + k + c that exit func or delete it if u want to
-                    //}
-                }
-                if (selected_cheat == 2) // Game 1
-                {
-                    //if (ImGui::Button("Inject", ImVec2(ImGui::GetWindowWidth() - 16, 23)))
-                    //{
-                    //    inject(L"csgo.exe", L"test.dll");
-                    //    exit(0); // if u want u can ctrl + k + c that exit func or delete it if u want to
+                    //    inject(L"YourGameName.Exe", L"YourCheatName.dll");
+                    //    /*exit(0);*/ // if u want u can ctrl + k + c that exit func or delete it if u want to
                     //}
                 }
                 ImGui::PopStyleVar();
